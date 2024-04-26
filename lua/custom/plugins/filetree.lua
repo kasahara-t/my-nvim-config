cd67043524b523a39dd44998aad0569853cdc7fb
kasahara-t <kasahara.tomoya.dev@gmail.com>
2024-04-26 16:41:23 +0900

    feat: 🎸 NeoTreeで隠しファイルや.から始まるフォルダが表示されるように変更

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -14,6 +14,19 @@ return {
         source_selector = {
           winbar = true,
         },
+        filesystem = {
+          filtered_items = {
+            visible = true,
+            show_hidden_count = true,
+            hide_dotfiles = false,
+            hide_gitignored = false,
+            hide_by_name = {
+              -- '.DS_Store',
+              -- 'thumbs.db',
+            },
+            never_show = { '.git' },
+          },
+        },
       }
     end
   end,
